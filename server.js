76b9adf1076f9491e9501112d85d68b671a20e91
Mark Dixon <mdixon@plymouth.ac.uk>
2022-11-08 11:31:07 +0000

    Added test endpoint.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", function(request, response) {
   response.send("Hello there!!!");
 });
 
+app.get("/test", function(request, response){
+  response.send("This is the test endpoint.");
+});
+
 app.listen(port, function() {
   console.log("Server listening on port " + port);
 });
